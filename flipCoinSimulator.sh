df4ff11f6a0fe32c774a7e83da898ed951a0899b
Prashik Kamble <kambleprashik7349@gmail.com>
2022-10-12 19:35:20 +0530

    [Prashik]Refactor the code to continue the loop till either of them have won 21 times.

--- flipCoinSimulator.sh
+++ flipCoinSimulator.sh
@@ -1,25 +1,44 @@
-#! /bin/bash/ -x
+##! /bin/bash/ -x
 
 isHead=1
+isTail=0
 
-headCount=0
-tailCount=0
+headWonTime=0
+tailWonTime=0
+fixedWonTime=21
 
 count=0
-while (( count < 10 ))
+
+while ((count < 42))
 do
 	flipCoin=$((RANDOM%2))
 
-	if [ $flipCoin -eq $isHead ]
+	if [[ ($flipCoin -eq $isHead) && ($headWonTime -lt $fixedWonTime) ]]
 	then
-		echo "Head is Winner"
-		((headCount++))
-	else
-		echo "Tail is Winner"
-		((tailCount++))
+		((headWonTime++))
+
+	elif [[ ($flipCoin -eq $isTail) && ($tailWonTime -lt $fixedWonTime) ]]
+	then
+		((tailWonTime++))
 	fi
 	((count++))
 done
 
-echo "Head Won $headCount times."
-echo "Tail Won $tailCount times."
+echo "Head Won $headWonTime times."
+echo "Tail Won $tailWonTime times."
+
+if [ $headWonTime -gt $tailWonTime ]
+then
+	headDiff=$(($headWonTime - $tailWonTime))
+	echo "Head is Won by $headDiff times more than Tail"
+
+elif [ $tailWonTime -gt $headWonTime ]
+then
+	tailDiff=$(($tailWonTime - $headWonTime))
+	echo "Tail is won by $tailDiff times more than Head"
+
+elif [ $headWonTime -eq $tailWonTime ]
+then
+	echo "Tie :- Both are won 21 times"
+
+fi
